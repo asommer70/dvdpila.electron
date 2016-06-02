2cb5dc72632de221dd46fbf8113cff45836edbb2
Adam Sommer <adam@thehoick.com>
2016-06-02 10:44:01 -0400

    Initial import.

---
+++ README.md
@@ -0,0 +1,16 @@
+# DVD Pila! Desktop
+
+A version of DVD Pila! Desktop developed using [Electron](http://electron.atom.io/).
+
+
+## Why?
+
+Because the Java version of DVD Pila! Desktop didn't easily play the [Matroska](https://matroska.org/) files I archived my DVD collection in.
+
+So a HTML5 video element in an Electron app should do just fine.
+
+Also, using [Foundation](http://foundation.zurb.com/sites/) and [Reat](https://facebook.github.io/react/) for the front-end.
+
+## Prerequisites
+
+You're going to need a local install of [DVD Pila!](http://dvdpila.thehoick.com/) on your network to point the desktop app to.  The JSON REST API is used to  list and play DVD archives.
---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var source = require('vinyl-source-stream');
+var browserify = require('browserify');
+var concat = require('gulp-concat');
+var watch = require('gulp-watch');
+var path = require('path');
+var babelify = require("babelify");
+var fs = require('fs');
+
+gulp.task('copy', function() {
+  gulp.src(['./src/index.html', './src/css/**/*', './src/js/**/*'], { "base" : "./src/." })
+  .pipe(gulp.dest('./build/'));
+})
+
+gulp.task('react', function() {
+  browserify("./src/app.js")
+  .transform("babelify", {presets: ["es2015", "react"]})
+  .bundle()
+  .pipe(fs.createWriteStream("./build/app.js"));
+});
+
+gulp.task('default', ['react', 'copy', 'watch']);
+
+gulp.task('watch', function () {
+  gulp.watch('./src/**/*.css', ['copy']);
+  gulp.watch('./src/**/*.js', ['react']);
+  gulp.watch('./src/**/*.html', ['copy']);
+});
