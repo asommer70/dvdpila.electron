8ae47da894eb2cdf3eaa7b6f09ce2d33880438d0
Adam Sommer <adam@thehoick.com>
2016-06-02 15:33:12 -0400

    Have the smallest area possible at the top working to drag the window around the screen... it'll do for now.

--- src/components/titlebar.js
+++ src/components/titlebar.js
@@ -7,18 +7,24 @@ export default class TitleBar extends Component {
 
   render() {
     return (
-      <div className="titlebar">
-        <div className="title">
+      <div className="titlebar" id="titleBar">
+        <div className="title" id="title">
           DVD Pila!
         </div>
-        <div className="row">
-          <div className="columns small-4 float-right">
-            <div className="input-group">
-              <input className="input-group-field search" type="text" />
-              <div className="input-group-button">
-                <input type="submit" className="button search" value="Search" />
-              </div>
+        <div className="row" id="titleLowerBar">
+          <div className="columns small-3 float-left nav-section">
+            <div className="button-group">
+              <a className="button tiny nav-btn">Back</a>
+              <a className="button tiny nav-btn">Forward</a>
             </div>
+          </div>
+
+          <div className="columns small-6 text-center add-dvd">
+            <a className="button tiny">Add DVD</a>
+          </div>
+
+          <div className="columns small-3 float-right">
+            <input className="input-group-field search" type="text" placeholder="Search" />
           </div>
         </div>
       </div>
